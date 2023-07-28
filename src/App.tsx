c901090b7b205c88b57f961d188035fe2c1ea467
Roshan <roshansuresh255@gmail.com>
2023-07-28 12:34:59 +0530

    navbar is build

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { Grid, GridItem, Show } from "@chakra-ui/react";
+import NavBar from "./components/NavBar";
 function app() {
   return (
     <Grid
@@ -7,8 +8,8 @@ function app() {
         lg: `'nav nav' 'leftside main'`,
       }}
     >
-      <GridItem area={"nav"} bg={"yellow"}>
-        nav
+      <GridItem area={"nav"}>
+        <NavBar />
       </GridItem>
       <Show above="lg">
         <GridItem area={"leftside"} bg={"green"}>
