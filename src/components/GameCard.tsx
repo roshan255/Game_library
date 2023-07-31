e5d96bd69fe2b4c99de153a1f78deef24375b01d
Roshan <roshansuresh255@gmail.com>
2023-07-31 19:24:47 +0530

    Displaying platform icons

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,5 +1,6 @@
-import { Card, CardBody, Heading, Image } from "@chakra-ui/react";
+import { Card, CardBody, Heading, Image, Text } from "@chakra-ui/react";
 import { gameResult } from "../hooks/useGame";
+import PlatformIcons from "./PlatformIcons";
 interface Props {
   game: gameResult;
 }
@@ -10,6 +11,9 @@ function GameCard({ game }: Props) {
       <Image src={game.background_image} />
       <CardBody>
         <Heading fontSize="2xl">{game.name}</Heading>
+        <PlatformIcons
+          platform={game.parent_platforms.map((p) => p.platform)}
+        />
       </CardBody>
     </Card>
   );
--- src/hooks/useGame.ts
+++ src/hooks/useGame.ts
@@ -2,11 +2,19 @@ import { useEffect, useState } from "react";
 import apiClient from "../services/api-client";
 import { CanceledError } from "axios";
 
+export interface Platform{
+  id:number;
+  name:string;
+  slug:string;
+}
+
 export interface gameResult {
     id: number;
     name: string;
     background_image:string;
+    parent_platforms:{platform:Platform}[];
   }
+
 interface fetchGameResponse {
     count: number;
     results: gameResult[];
