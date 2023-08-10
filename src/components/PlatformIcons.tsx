d2c2f1a6a64f3f64df5bc293d441a698bb01803b
Roshan <roshansuresh255@gmail.com>
2023-08-10 12:12:48 +0530

    handling errors while building

--- src/components/PlatformIcons.tsx
+++ src/components/PlatformIcons.tsx
@@ -10,7 +10,7 @@ import { MdPhoneIphone } from "react-icons/md";
 import { SiNintendo } from "react-icons/Si";
 import { BsGlobe } from "react-icons/bs";
 import { HStack, Icon, Text } from "@chakra-ui/react";
-import { Platform } from "../hooks/useGame";
+import { Platform } from "../hooks/usePlatform";
 import { IconType } from "react-icons";
 
 interface Props {
